b1227e5624b68baf7e1073a3755f90a4f8c4f5f3
nurulcse7 <nurul.cse7@gmail.com>
2022-09-11 12:41:47 +0600

    Updated  history file

---
+++ history.js
@@ -0,0 +1,12 @@
+history.back // previous page load
+history.forward // next page load 
+history.go(-2)
+history.go(-1) // previous page load
+history.go(0) // current page reload
+history.go(1) // next page load 
+history.go(2)
+history.length   // total history page length
+history.pushState
+history.replaceState
+history.scrollRestoration
+history.state
